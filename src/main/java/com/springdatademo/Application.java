677ca2ad7420d0be6fff408ac694fb5a93faff2f
Michael Wiles <michael.wiles@afrozaar.com>
2016-01-25 23:12:02 +0200

    remove main method from application clasass

--- src/main/java/com/springdatademo/Application.java
+++ src/main/java/com/springdatademo/Application.java
@@ -1,4 +1,5 @@
 package com.springdatademo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -7,17 +8,5 @@ import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
-
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
-    }
 
 }
